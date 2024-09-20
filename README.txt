1695c19d07b8ba32bcbab52c3004ed5e4f313d49
RapolasRimeika <118994312+RapolasRimeika@users.noreply.github.com>
2024-09-20 22:05:05 +0100

    adding player.py to define the player (circle for hitpoints) triangle for drawing

--- README.txt
+++ README.txt
@@ -3,3 +3,8 @@ Also learning to use multiple files that reference each other.
 It uses pygame library (insert link here)
 
 Implementing FPS with pygame.time.Clock() 
+using pygame.sprite.sprite to create a circle class
+creating a player sub class that calls circle shape class to initialise the player with circle class with the radius and x and y coordinates.
+The circle shape is only used for hitpoints
+The player.py defines the player class 
+draws the roatating triangle that represents the ship
---
+++ player.py
@@ -0,0 +1,20 @@
+class Player(CircleShape):
+    def__init__(self, x, y)
+    super().__init__(PLAYER_RADIUS)
+    
+#defining the triangle shape that rotates    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+# Draws a triangle shape on the screen using pygame's draw.polygon method
+# - screen: The surface to draw the triangle on (passed in as an argument)
+# - "white": The color of the triangle's outline
+# - self.triangle(): A list of points representing the vertices of the triangle
+# - 2: The width of the triangle's outline (if 0, it would fill the shape)
+def draw(self, screen):
+    self.pygame.draw.polygon(screen, "white", self.triangle(), 2)
