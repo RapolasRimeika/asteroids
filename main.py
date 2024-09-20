1bb4d79b7c3d7f0af70a89c9e3e7ab44511ebf5a
RapolasRimeika <118994312+RapolasRimeika@users.noreply.github.com>
2024-09-20 20:52:25 +0100

    Updating game logic with FPS

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ from constants import *
 
 print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
-color = (0, 0, 0)
-running = True
-while running:
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
 
-#this will check if player closed the window and stop the game loop if they do.
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
